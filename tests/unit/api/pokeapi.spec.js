162e02294e6a5f909a08cded8fa23ef8576843d5
Luis Angel <angel.rh.2108@gmail.com>
2022-11-29 13:57:39 -0600

    Pruebas Unitarias - Axios

---
+++ tests/unit/api/pokeapi.spec.js
@@ -0,0 +1,12 @@
+import pokeApi from "@/api/pokeapi";
+
+describe("api",() =>
+{
+    /**
+     * La URL de axios debe ser la correcta
+     */
+    test("Axios debe estar configurado correctamente",() =>
+    {
+        expect(pokeApi.defaults.baseURL).toBe("https://pokeapi.co/api/v2/pokemon1")
+    });
+})
